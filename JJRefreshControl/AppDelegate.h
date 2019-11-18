b23508dbbede475c28898803f310584010119dbf
xiaotao <937840988@qq.com>
2019-11-18 22:22:30 +0800

    Initial Commit

---
+++ JJRefreshControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJRefreshControl
+//
+//  Created by RJ on 2019/11/18.
+//  Copyright © 2019年 RJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJRefreshControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJRefreshControl
+//
+//  Created by RJ on 2019/11/18.
+//  Copyright © 2019年 RJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
